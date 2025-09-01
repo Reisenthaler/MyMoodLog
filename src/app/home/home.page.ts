d075d36ddcfe963551749fb58593203f093a83be
Reisenthaler <tobiasreisenthaler@gmail.com>
2025-09-01 21:12:02 +0200

    feat(home): add overlay info components with backdrop dismiss and rounded close button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   IonHeader,
   IonToolbar,
@@ -7,21 +8,64 @@ import {
   IonList,
   IonItem,
   IonLabel,
+  IonIcon,
+  IonButton,
 } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core'; 
+import { TranslateModule } from '@ngx-translate/core';
+import { CrisisPlanInfoComponent } from '../components/crisis-plan-info/crisis-plan-info.component';
+import { MoodLogInfoComponent } from '../components/mood-log-info/mood-log-info.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { addIcons } from 'ionicons';
+import { informationCircleOutline } from 'ionicons/icons';
+addIcons({
+  informationCircleOutline,
+});
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, TranslateModule],
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    TranslateModule,
+    IonIcon,
+    IonButton,
+    CrisisPlanInfoComponent,
+    MoodLogInfoComponent,
+    ButtonComponent,
+  ],
 })
 export class HomePage {
+  showCrisisPlanInfo = false;
+  showMoodLogInfo = false;
+
   constructor(private router: Router) {}
 
   goTo(path: string) {
     this.router.navigateByUrl('/' + path);
+  }
+
+  openCrisisPlanInfo(event: Event) {
+    event.stopPropagation();
+    this.showCrisisPlanInfo = true;
+  }
+
+  openMoodLogInfo(event: Event) {
+    event.stopPropagation();
+    this.showMoodLogInfo = true;
+  }
+
+  closeInfo() {
+    this.showCrisisPlanInfo = false;
+    this.showMoodLogInfo = false;
   }
 }
