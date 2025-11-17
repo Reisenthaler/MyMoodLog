e73d310bf8e3eb54bcdc9ce17fe71f4b51390905
Reisenthaler <tobias.reisenthaler@gmail.com>
2025-11-17 21:32:02 +0100

    fix: make appId and android package unique for PlayStore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.reisenthaler.mymoodlog',
   appName: 'MyMoodLog',
   webDir: 'www'
 };
